4a424a6e686c8931d220766065f2c7419bb75230
Francisco M. <f.molinamontenegro@gmail.com>
2026-06-05 01:05:49 +0200

    remove deprecated endpoint

--- frontend/src/constants/endpoints.js
+++ frontend/src/constants/endpoints.js
@@ -1,7 +1,6 @@
 export const API_BASE_URL = 'http://localhost:8000';
 export const ENDPOINTS = {
   ANALYZE_CV: '/analyze-cv/',
-  ANALYZE_TEXT: '/analyze-text/',
   EXPORT_REPORT: '/export-report/',
   DOWNLOAD_PDF_REPORT: '/api/download-pdf-report/',
 };
